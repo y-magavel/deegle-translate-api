315649f44b65b900180a2448d511daa8e045f4f0
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-07 00:15:10 +0900

    mainをできるだけシンプルになるように変更 mainを最低限の処理のみを書くように修正

--- src/main.go
+++ src/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/tmsic/deegle-translate-api/db"
 	"github.com/tmsic/deegle-translate-api/router"
 )
 
 func main() {
 
-	db.DBConnect()
 	// ルータ、ミドルウェアを設定
 	r := router.GetRouter()
-
-	r.Run()
+		r.Run()
 }
