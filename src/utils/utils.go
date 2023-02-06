3bdcc8644e96d1351af5304e28b149b41861d291
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-07 00:17:10 +0900

    DB接続処理を別関数にまかせる 記述が多くなるので、DB関係処理を別ファイルに移動

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,8 +1,39 @@
 package utils
 
-func WriteJSON() {
-}
-func ReadJSON() {
-}
-func ErrorJSON() {
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	// "github.com/joho/godotenv"
+)
+
+func OpenDB() *gorm.DB {
+
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	fmt.Printf("読み込み出来ませんでした: %v", err)
+	// }
+	// .envの SAMPLE_MESSAGEを取得して、messageに代入します。
+	// dbms := os.Getenv("DBMS")
+	// dbUser := os.Getenv("USER")
+	// dbPass := os.Getenv("PASS")
+	// protocol := os.Getenv("PROTOCOL")
+	// dbName := os.Getenv("DBNAME")
+	// CONNECT := dbUser + ":" + dbPass + "@" + protocol + "/" + dbName + "?parseTime=true"
+	// db, err := gorm.Open(dbms, CONNECT)
+
+	DBMS := "mysql"
+	USER := "deegle"
+	PASS := "deegle"
+	PROTOCOL := "tcp(deegle-db:3306)"
+	DBNAME := "deegle"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		fmt.Println("接続失敗")
+		panic(err.Error())
+	}
+
+	return db
 }
